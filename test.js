a2dcc75ec1cd865a057b91754def763d645758e4
Madranges Lucas <lucasmadranges06@gmail.com>
2022-12-14 20:35:44 +0100

    First commit

---
+++ test.js
@@ -0,0 +1,14 @@
+function test()
+{
+    document.getElementById("demo").innerHTML = "LOL MAXIME";
+}
+
+function ampoule_allumer()
+{
+    document.getElementById('ampoule').src='images/ampoule_allume.png'
+}
+
+function ampoule_eteint()
+{
+    document.getElementById('ampoule').src='images/ampoule_eteint.png'
+}
